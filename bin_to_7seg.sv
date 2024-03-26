cdfa5f49b53b1a39b855113d452cc38f5c5eae45
Laplace Darknesss <133151430+notkuroneko@users.noreply.github.com>
2024-03-26 21:41:07 +0700

    Add files via upload

--- bin_to_7seg.sv
+++ bin_to_7seg.sv
@@ -1,156 +1,26 @@
 module bin_to_7seg (
 	input [3:0] w_bcd,
-	output reg[6:0] w_seg7
+	output logic [6:0] w_seg7
 );
-reg [3:0] s;
 	always @(w_bcd) 
 	begin
-		s = w_bcd[3]*8 + w_bcd[2]*4 + w_bcd[1]*2 + w_bcd[1];	
-		case (s)
-			4'h0: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 0;
-			end
-			4'h1: begin
-				w_seg7[6] = 0;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 0;
-				w_seg7[2] = 0;
-				w_seg7[1] = 0;
-				w_seg7[0] = 0;
-			end
-			4'h2: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 0;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 0;
-				w_seg7[0] = 1;
-			end
-			4'h3: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 0;
-				w_seg7[1] = 0;
-				w_seg7[0] = 1;
-			end
-			4'h4: begin
-				w_seg7[6] = 0;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 0;
-				w_seg7[2] = 0;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'h5: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 0;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 0;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'h6: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 0;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'h7: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 0;
-				w_seg7[2] = 0;
-				w_seg7[1] = 0;
-				w_seg7[0] = 0;
-			end
-			4'h8: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'h9: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 0;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'hA: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 0;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'hB: begin
-				w_seg7[6] = 0;
-				w_seg7[5] = 0;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'hC: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 0;
-				w_seg7[4] = 0;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 0;
-			end
-			4'hD: begin
-				w_seg7[6] = 0;
-				w_seg7[5] = 1;
-				w_seg7[4] = 1;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 0;
-				w_seg7[0] = 1;
-			end
-			4'hE: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 0;
-				w_seg7[4] = 0;
-				w_seg7[3] = 1;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
-			4'hF: begin
-				w_seg7[6] = 1;
-				w_seg7[5] = 0;
-				w_seg7[4] = 0;
-				w_seg7[3] = 0;
-				w_seg7[2] = 1;
-				w_seg7[1] = 1;
-				w_seg7[0] = 1;
-			end
+		case (w_bcd)
+			4'h0: w_seg7 = 7'd62;
+			4'h1: w_seg7 = 7'd48;
+			4'h2: w_seg7 = 7'd109;
+			4'h3: w_seg7 = 7'd121;
+			4'h4: w_seg7 = 7'd51;
+			4'h5: w_seg7 = 7'd91;
+			4'h6: w_seg7 = 7'd95;
+			4'h7: w_seg7 = 7'd112;
+			4'h8: w_seg7 = 7'd127;
+			4'h9: w_seg7 = 7'd123;
+			4'hA: w_seg7 = 7'd119;
+			4'hB: w_seg7 = 7'd31;
+			4'hC: w_seg7 = 7'd78;
+			4'hD: w_seg7 = 7'd61;
+			4'hE: w_seg7 = 7'd79;
+			4'hF: w_seg7 = 7'd71;
 		endcase
 	end
 endmodule
@@ -168,21 +38,21 @@ module bin_to_7seg_test();
 		stim_bcd = 4'd13;
 		#5 stim_bcd = 4'd4;
 		#5 stim_bcd = 4'd8;
-	endc
+	end
 endmodule
-// 0000 -> 1111110
-// 0001 -> 0110000
-// 0010 -> 1101101
-// 0011 -> 1111001
-// 0100 -> 0110011
-// 0101 -> 1011011
-// 0110 -> 1011111
-// 0111 -> 1110000
-// 1000 -> 1111111
-// 1001 -> 1111011
-// 1010 -> 1110111
-// 1011 -> 0011111
-// 1100 -> 1001110
-// 1101 -> 0111101
-// 1110 -> 1001111
-// 1111 -> 1000111
+// 0000 -> 1111110 = 62
+// 0001 -> 0110000 = 48
+// 0010 -> 1101101 = 109
+// 0011 -> 1111001 = 121
+// 0100 -> 0110011 = 51
+// 0101 -> 1011011 = 91
+// 0110 -> 1011111 = 95
+// 0111 -> 1110000 = 112
+// 1000 -> 1111111 = 127
+// 1001 -> 1111011 = 123
+// 1010 -> 1110111 = 119
+// 1011 -> 0011111 = 31
+// 1100 -> 1001110 = 78
+// 1101 -> 0111101 = 61
+// 1110 -> 1001111 = 79
+// 1111 -> 1000111 = 71 
