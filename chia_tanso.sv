a542cfb49adb57848d03109c6e8100e4a087a3b0
Anh Ho Pham <128297495+pah69@users.noreply.github.com>
2024-03-27 12:48:09 +0700

    Update chia_tanso.sv

--- chia_tanso.sv
+++ chia_tanso.sv
@@ -1,23 +1,22 @@
 module chia_tanso (
     input wire clk_50MHz,
-    output reg clk_1Hz
+    output reg tick
 );
 
 reg [25:0] counter; // 26-bit counter, for 50MHz to 1Hz
-reg [3:0] digit; // 4-bit counter, count from 0 to 9
 
 always @(posedge clk_50MHz) begin
-    if (counter == 50000000 - 1) begin // dem tu 0 nen phai tru 1
-        counter <= 0;
-        if (digit == 9) begin
-            digit <= 0; // Reset khi dem den 9
-            clk_1Hz <= ~clk_1Hz; // Flip bit 
-        end else begin
-            digit <= digit + 1; // Tang so dem
-        end
-    end else begin
-        counter <= counter + 1;
-    end
+    if (counter == 50000000 - 1) begin
+	counter <= 0;
+	if (counter[3:0] == 9) begin
+	tick <= 1; 
+	end else begin
+	tick <= 0;
+	end
+	end else begin
+	counter <= counter + 1; 
+	tick <= 0;
+	end
 end
 
 endmodule
