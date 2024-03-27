ee8cc0b2de991db15890b2a0d60b1274e9620199
Anh Ho Pham <128297495+pah69@users.noreply.github.com>
2024-03-27 18:49:47 +0700

    Update chia_tanso.sv
    finish

--- chia_tanso.sv
+++ chia_tanso.sv
@@ -1,22 +1,21 @@
-module chia_tanso (
+module clock_divider (
     input wire clk_50MHz,
-    output reg tick
+    input wire reset_n,
+    output tick
 );
 
-reg [25:0] counter; // 26-bit counter, for 50MHz to 1Hz
-
-always @(posedge clk_50MHz) begin
-    if (counter == 50000000 - 1) begin
-	counter <= 0;
-	if (counter[3:0] == 9) begin
-	tick <= 1; 
-	end else begin
-	tick <= 0;
-	end
-	end else begin
-	counter <= counter + 1; 
-	tick <= 0;
-	end
+reg [25:0] counter;
+always @(posedge clk_50MHz or negedge reset_n) begin
+    if (~reset_n) begin
+        counter <= 0; // Reset bo dem
+    if (counter  == 50000000 - 1) begin // 50MHz -> 1Hz, cong thuc 
+        counter <= 0;
+    end else begin
+        counter <= counter + 1; // cong them 1 moi lan dem
+    end
 end
+
+assign tick = counter == 500000000 - 1;
+
 
 endmodule
